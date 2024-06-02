40b9ac7f5084eb452afb14d5888ed65bc1852072
SidrorovVY <anenvs@mail.ru>
2024-06-02 22:04:27 +0300

    Домашняя работа. Урок 5

---
+++ src/main/java/ru/otus/homework5/animals/Cat.java
@@ -0,0 +1,11 @@
+package ru.otus.homework5.animals;
+
+public class Cat extends Animal{
+    public Cat(String name, int vitality) {
+        this.name = name;
+        this.runSpeed = 1;
+        this.swimSpeed = 0;
+        this.vitality = vitality;
+        this.fatigue = fatigue;
+    }
+}
